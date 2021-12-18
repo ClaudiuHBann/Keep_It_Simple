3f3178673ed7799e55e32a515fc092c89cc8e2c8
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2021-12-17 16:30:36 -0800

    Create Radar Hack.cpp

---
+++ CS GO External Cheat/Radar Hack.cpp
@@ -0,0 +1,97 @@
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <string>
+
+#define dwEntityList 0x4DCFA94
+#define m_bSpotted 0x93D
+
+template<typename T>
+inline T RPM(HANDLE handle, const uintptr_t address)
+{
+	T t;
+	ReadProcessMemory(handle, (LPCVOID)address, &t, sizeof(t), nullptr);
+
+	return t;
+}
+
+template<typename T>
+inline BOOL WPM(HANDLE handle, const uintptr_t address, const T& t)
+{
+	return WriteProcessMemory(handle, (LPVOID)address, &t, sizeof(t), nullptr);
+}
+
+HMODULE GetModuleHandle(DWORD pid, const std::string& name)
+{
+	HANDLE snapshotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+	MODULEENTRY32 moduleEntry;
+	moduleEntry.dwSize = sizeof(moduleEntry);
+
+	if (!Module32First(snapshotHandle, &moduleEntry))
+	{
+		CloseHandle(snapshotHandle);
+		return 0;
+	}
+
+	do
+	{
+		if (std::equal(name.begin(), name.end(), moduleEntry.szModule))
+		{
+			CloseHandle(snapshotHandle);
+			return moduleEntry.hModule;
+		}
+	} while (Module32Next(snapshotHandle, &moduleEntry));
+
+	CloseHandle(snapshotHandle);
+	return 0;
+}
+
+DWORD GetProcessID(const std::string& name)
+{
+	HANDLE snapshotHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 processEntry;
+	processEntry.dwSize = sizeof(processEntry);
+
+	if (!Process32First(snapshotHandle, &processEntry))
+	{
+		CloseHandle(snapshotHandle);
+		return 0;
+	}
+
+	do
+	{
+		if (std::equal(name.begin(), name.end(), processEntry.szExeFile))
+		{
+			CloseHandle(snapshotHandle);
+			return processEntry.th32ProcessID;
+		}
+	} while (Process32Next(snapshotHandle, &processEntry));
+
+	CloseHandle(snapshotHandle);
+	return 0;
+}
+
+int main()
+{
+	DWORD csgoPID = GetProcessID("csgo.exe");
+	HANDLE csgoHandle = OpenProcess(PROCESS_ALL_ACCESS, false, csgoPID);
+
+	HMODULE csgoHandleClient = GetModuleHandle(csgoPID, "client.dll");
+
+	while (true)
+	{
+		for (uint8_t i = 0; i < 64; i++)
+		{
+			DWORD entity = RPM<DWORD>(csgoHandle, (uintptr_t)csgoHandleClient + dwEntityList + i * 0x10);
+
+			if (entity)
+			{
+				WPM<bool>(csgoHandle, entity + m_bSpotted, true);
+			}
+		}
+
+		Sleep(1);
+	}
+
+
+	return 0;
+}
